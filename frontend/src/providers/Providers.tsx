1bee6318ee794563367c0207f669d6e2eed89114
cats256 <59489624+cats256@users.noreply.github.com>
2023-12-29 19:53:11 -0600

    Fix the flickering issue when starting app

--- frontend/src/providers/Providers.tsx
+++ frontend/src/providers/Providers.tsx
@@ -1,9 +1,15 @@
 "use client";
+import { ColorModeScript } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../chakra/theme";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+    return (
+        <>
+            <ColorModeScript initialColorMode="dark" />
+            <ChakraProvider theme={theme}>{children}</ChakraProvider>
+        </>
+    );
 }
 
 export default Providers;
