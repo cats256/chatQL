3df7984ca8f47ae10dd9956280ae7c72e9dbd7be
cats256 <59489624+cats256@users.noreply.github.com>
2023-12-29 20:08:01 -0600

    Remove redundant component

--- frontend/src/components/AuthButton.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { getSession, signIn, signOut } from "next-auth/react";
-
-export default function AuthButton(session) {
-    return <>{session?.user ? <button onClick={() => signOut()}>Sign Out</button> : <button onClick={() => signIn("google")}>Sign In</button>}</>;
-}
