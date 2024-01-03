0e11f78b7ee57b7b96951c9a0dfb0ced55cee1bf
cats256 <59489624+cats256@users.noreply.github.com>
2024-01-02 22:19:11 -0600

    Create createConversation resolver

--- backend/src/graphql/resolvers/conversation.ts
+++ backend/src/graphql/resolvers/conversation.ts
@@ -1,9 +1,60 @@
+import { GraphQLError } from "graphql";
 import { GraphQLContext } from "../../util/types";
+import { ApolloError } from "apollo-server-core";
 
 const resolvers = {
     Mutation: {
         createConversation: async (_: any, args: { participantsIds: Array<string> }, context: GraphQLContext) => {
-            console.log("createConversation called", args);
+            const { session, prisma } = context;
+            const { participantsIds } = args;
+
+            if (!session?.user) {
+                throw new ApolloError("Not authorized.");
+            }
+
+            const {
+                user: { id: userId },
+            } = session;
+
+            try {
+                const conversation = await prisma.conversation.create({
+                    data: {
+                        participants: {
+                            createMany: {
+                                data: participantsIds.map((participantId) => ({
+                                    userId: participantId,
+                                    hasSeenLatestMessage: participantId === userId,
+                                })),
+                            },
+                        },
+                    },
+                    include: {
+                        participants: {
+                            include: {
+                                user: {
+                                    select: {
+                                        id: true,
+                                        username: true,
+                                    },
+                                },
+                            }
+                        },
+                        latestMessage: {
+                            include: {
+                                sender: {
+                                    select: {
+                                        id: true,
+                                        username: true,
+                                    }
+                                }
+                            }
+                        }
+                    },
+                });
+            } catch (error) {
+                console.error("createConversation error", error);
+                throw new GraphQLError("Error creating conversation");
+            }
         },
     },
 };
