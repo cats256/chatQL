78c8c7b42e38e6ccbbb32b69a0c25eafd10ee0ab
cats256 <59489624+cats256@users.noreply.github.com>
2024-01-02 22:41:36 -0600

    Finish up resolver and refactor

--- backend/src/graphql/resolvers/conversation.ts
+++ backend/src/graphql/resolvers/conversation.ts
@@ -1,10 +1,40 @@
 import { GraphQLError } from "graphql";
 import { GraphQLContext } from "../../util/types";
 import { ApolloError } from "apollo-server-core";
+import { Prisma } from "@prisma/client";
+
+export const participantPopulated = Prisma.validator<Prisma.ConversationParticipantInclude>()({
+    user: {
+        select: {
+            id: true,
+            username: true,
+        },
+    },
+});
+
+export const conversationPopulated = Prisma.validator<Prisma.ConversationInclude>()({
+    participants: {
+        include: participantPopulated,
+    },
+    latestMessage: {
+        include: {
+            sender: {
+                select: {
+                    id: true,
+                    username: true,
+                },
+            },
+        },
+    },
+});
 
 const resolvers = {
     Mutation: {
-        createConversation: async (_: any, args: { participantsIds: Array<string> }, context: GraphQLContext) => {
+        createConversation: async (
+            _: any,
+            args: { participantsIds: Array<string> },
+            context: GraphQLContext
+        ): Promise<{ conversationId: string }> => {
             const { session, prisma } = context;
             const { participantsIds } = args;
 
@@ -28,29 +58,12 @@ const resolvers = {
                             },
                         },
                     },
-                    include: {
-                        participants: {
-                            include: {
-                                user: {
-                                    select: {
-                                        id: true,
-                                        username: true,
-                                    },
-                                },
-                            }
-                        },
-                        latestMessage: {
-                            include: {
-                                sender: {
-                                    select: {
-                                        id: true,
-                                        username: true,
-                                    }
-                                }
-                            }
-                        }
-                    },
+                    include: conversationPopulated,
                 });
+
+                return {
+                    conversationId: conversation.id,
+                };
             } catch (error) {
                 console.error("createConversation error", error);
                 throw new GraphQLError("Error creating conversation");
